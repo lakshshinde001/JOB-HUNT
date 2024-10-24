de73a1fb437a88d0ff2d8d10cff4313a1fe0515b
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-10-24 19:29:20 +0530

    Designed Homepage, added HeroSection, LatestJobs, LatestJobCard & category carausel & footer

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarausal from './CategoryCarausal'
+import LatestJobs from './LatestJobs'
+import Footer from './shared/Footer'
 
 const Home = () => {
-  return (
+  return (  
     <div>
         <Navbar/>
+        <HeroSection/>
+        <CategoryCarausal/>
+        <LatestJobs/>
+        <Footer/>
     </div>
   )
 }
