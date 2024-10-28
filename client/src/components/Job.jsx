4475065773e4d85ed630bd86b7730ffb375b74f6
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-10-28 11:09:03 +0530

    Job description frontend done

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Button } from './ui/button'
 import { Badge, Bookmark } from 'lucide-react'
 import { Avatar, AvatarImage } from './ui/avatar'
+import { useNavigate } from 'react-router-dom'
 
 function Job() {
+    const navigate = useNavigate();
+    const jobId = "asdfljfaalksdjflkj"
   return (
     <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
             <div className='flex items-center justify-between'>
@@ -33,7 +36,7 @@ function Job() {
                 <Badge className={'text-[#7209b7] font-bold'} variant="ghost">10LPA</Badge>
             </div>
             <div className='flex items-center gap-4 mt-4'>
-                <Button variant="outline">Details</Button>
+                <Button onClick={()=> navigate(`/description/${jobId}`)} variant="outline">Details</Button>
                 <Button className="bg-[#7209b7]">Save For Later</Button>
             </div>
         </div>
